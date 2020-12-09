50914d405ce94797fa731189ff11faed076cb1fc
Blaise Liu <blaise.liu@egov.com>
2020-12-09 11:02:05 -0500

    Add project files.

---
+++ RouterCommand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RouterCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
